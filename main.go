ca80419383843a06328d71da27224cfcc5105126
atec <root@atec.pub>
2019-03-05 13:20:59 -0500

    poc

--- main.go
+++ main.go
@@ -17,15 +17,38 @@ type database struct {
 	deleteBuf []string
 }
 
-func (db database) get(name string) *string {
-	// TODO behavior during a tx?
-	if val, ok := db.store[name]; ok {
-		return &val
+func (db *database) get(name string) *string {
+
+	// TODO maybe dedup. but rn it clearly
+	// .    bifurcates the cases
+	if db.tx {
+
+		// check if we deleted it in this tx
+		for _, delName := range db.deleteBuf {
+			if name == delName {
+				return nil
+			}
+		}
+
+		// check if we set it in this tx
+		if val, ok := db.setBuf[name]; ok {
+			return &val
+		}
+
+		if val, ok := db.store[name]; ok {
+			return &val
+		}
+
+	} else {
+		if val, ok := db.store[name]; ok {
+			return &val
+		}
 	}
+
 	return nil
 }
 
-func (db database) set(name, val string) {
+func (db *database) set(name, val string) {
 	if db.tx {
 		db.setBuf[name] = val
 	} else {
@@ -33,32 +56,70 @@ func (db database) set(name, val string) {
 	}
 }
 
-func (db database) delete(name string) {
+func (db *database) delete(name string) {
+
+	// TODO no-op proper semantics when name doesn't exist?
+
 	if db.tx {
+
+		// check if we set the key in this transaction
+		if _, ok := db.setBuf[name]; ok {
+			delete(db.setBuf, name)
+			return
+		}
+
+		// otherwise queue up a delete
 		db.deleteBuf = append(db.deleteBuf, name)
+
 	} else {
 		delete(db.store, name)
 	}
 }
 
-func (db database) count(val string) uint {
+func (db *database) count(val string) uint {
 	var count uint
-	for _, value := range db.store {
-		if val == value {
-			count++
+	if db.tx {
+
+		for _, value := range db.setBuf {
+			if value == val {
+				count++
+			}
+		}
+
+		for name, value := range db.store {
+			if value == val {
+
+				// if we are about to delete this name
+				// don't count it
+				if contains(db.deleteBuf, name) {
+					continue
+				}
+
+				count++
+			}
+		}
+
+	} else {
+		for _, value := range db.store {
+			if val == value {
+				count++
+			}
 		}
 	}
 	return count
 }
 
-func (db database) rollback() error {
+func (db *database) begin() {
+	db.tx = true
+}
+
+func (db *database) rollback() {
 	db.setBuf = make(map[string]string)
 	db.deleteBuf = nil
 	db.tx = false
-	return nil
 }
 
-func (db database) commit() error {
+func (db *database) commit() {
 	for name, val := range db.setBuf {
 		db.store[name] = val
 	}
@@ -68,12 +129,19 @@ func (db database) commit() error {
 	}
 	db.setBuf = make(map[string]string)
 	db.deleteBuf = nil
-	return nil
+	db.tx = false
 }
 
 // TODO usage
 func err() {
 	println("invalid input")
+}
+
+func debug(db database) {
+	pretty.Println("store:", db.store)
+	pretty.Println("tx:", db.tx)
+	pretty.Println("setBuf:", db.setBuf)
+	pretty.Println("delBuf:", db.deleteBuf)
 }
 
 func main() {
@@ -91,17 +159,20 @@ func main() {
 
 		args := strings.Split(stripped, " ")
 
+		// TODO add debug flag
+		// println()
+		// println("before...")
+		// debug(db)
+
 		switch len(args) {
 		case 1:
 			switch strings.ToLower(args[0]) {
 			case "begin":
-				db.tx = true
+				db.begin()
 			case "commit":
 				db.commit()
-				db.tx = false
 			case "rollback":
 				db.rollback()
-				db.tx = false
 			case "end":
 				os.Exit(0)
 			default:
@@ -110,7 +181,6 @@ func main() {
 		case 2:
 			switch strings.ToLower(args[0]) {
 			case "get":
-				pretty.Println(db.store)
 				val := db.get(args[1])
 				if val == nil {
 					println("<nil>") // TODO this could actually be a val
@@ -127,12 +197,27 @@ func main() {
 		case 3:
 			if strings.ToLower(args[0]) == "set" {
 				db.set(args[1], args[2])
-				pretty.Println(db.store)
 			} else {
 				err()
 			}
 		default:
 			err()
 		}
+
+		// TODO add debug flag
+		// println()
+		// println("after...")
+		// debug(db)
+
+		// println()
 	}
+}
+
+func contains(strs []string, val string) bool {
+	for _, str := range strs {
+		if str == val {
+			return true
+		}
+	}
+	return false
 }
