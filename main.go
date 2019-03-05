cfa4b88d1716504033d8204b36be2af692024098
atec <root@atec.pub>
2019-03-05 12:46:23 -0500

    poc for bufs backing txs

--- main.go
+++ main.go
@@ -2,60 +2,73 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/kr/pretty"
 )
-
-type db interface {
-	set(name, val string) error
-	get(name string) (val string, err error)
-	delete(name string) (val string, err error) // TODO maybe just return err?
-	count(val string) (count uint, err error)
-
-	end() error
-
-	begin() error
-	rollback() error
-	commit() error
-}
 
 type database struct {
 	store map[string]string
 
-	buf map[string]string
-	txs []tx
+	tx        bool
+	setBuf    map[string]string
+	deleteBuf []string
 }
 
-func (db database) get(name string) (*string, error) {
+func (db database) get(name string) *string {
+	// TODO behavior during a tx?
 	if val, ok := db.store[name]; ok {
-		return &val, nil
+		return &val
 	}
-	return nil, errors.New("not found")
-}
-
-func (db database) set(name, val string) error {
-	db.store[name] = val
 	return nil
 }
 
-func (db database) delete(name string) error {
-	delete(db.store, name)
-	return nil
+func (db database) set(name, val string) {
+	if db.tx {
+		db.setBuf[name] = val
+	} else {
+		db.store[name] = val
+	}
 }
 
-func (db database) count(val string) (uint, error) {
+func (db database) delete(name string) {
+	if db.tx {
+		db.deleteBuf = append(db.deleteBuf, name)
+	} else {
+		delete(db.store, name)
+	}
+}
+
+func (db database) count(val string) uint {
 	var count uint
 	for _, value := range db.store {
 		if val == value {
 			count++
 		}
 	}
-	return count, nil
+	return count
 }
 
-type tx struct {
+func (db database) rollback() error {
+	db.setBuf = make(map[string]string)
+	db.deleteBuf = nil
+	db.tx = false
+	return nil
+}
+
+func (db database) commit() error {
+	for name, val := range db.setBuf {
+		db.store[name] = val
+	}
+	for _, name := range db.deleteBuf {
+		// TODO maybe drain buffer first
+		delete(db.store, name)
+	}
+	db.setBuf = make(map[string]string)
+	db.deleteBuf = nil
+	return nil
 }
 
 // TODO usage
@@ -67,6 +80,7 @@ func main() {
 
 	db := database{}
 	db.store = make(map[string]string)
+	db.setBuf = make(map[string]string)
 
 	r := bufio.NewReader(os.Stdin)
 
@@ -81,8 +95,14 @@ func main() {
 		case 1:
 			switch strings.ToLower(args[0]) {
 			case "begin":
+				db.tx = true
+			case "commit":
+				db.commit()
+				db.tx = false
 			case "rollback":
-			case "end": // TODO maybe strip trailing newline
+				db.rollback()
+				db.tx = false
+			case "end":
 				os.Exit(0)
 			default:
 				err()
@@ -90,32 +110,24 @@ func main() {
 		case 2:
 			switch strings.ToLower(args[0]) {
 			case "get":
-				// pretty.Println(db.store)
-				val, err := db.get(args[1])
-				if err != nil {
+				pretty.Println(db.store)
+				val := db.get(args[1])
+				if val == nil {
 					println("<nil>") // TODO this could actually be a val
 				} else {
-					fmt.Println(*val) // TODO could this be nil?
+					fmt.Println(*val)
 				}
 			case "delete":
-				if err := db.delete(args[1]); err != nil {
-					println("this shouldn't happen")
-				}
+				db.delete(args[1])
 			case "count":
-				n, err := db.count(args[1])
-				if err != nil {
-					println("this shouldn't happen")
-				}
-				fmt.Println(n)
+				fmt.Println(db.count(args[1]))
 			default:
 				err()
 			}
 		case 3:
 			if strings.ToLower(args[0]) == "set" {
-				if err := db.set(args[1], args[2]); err != nil {
-					println("this shouldn't happen")
-				}
-				// pretty.Println(db.store)
+				db.set(args[1], args[2])
+				pretty.Println(db.store)
 			} else {
 				err()
 			}
