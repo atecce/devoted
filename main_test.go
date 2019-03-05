9435e10eb8cd749c149fecb809ded109a1c9c978
atec <root@atec.pub>
2019-03-05 14:01:41 -0500

    test 4 fails. need nested stack of transactions

--- main_test.go
+++ main_test.go
@@ -115,3 +115,69 @@ func TestExample3(t *testing.T) {
 		t.Errorf("val should be nil, got %v", val)
 	}
 }
+
+func TestExample4(t *testing.T) {
+
+	db := database{}
+	db.store = make(map[string]string)
+	db.setBuf = make(map[string]string)
+
+	var val *string
+	var n uint
+
+	db.set("a", "foo")
+	db.set("b", "baz")
+
+	db.begin()
+
+	val = db.get("a")
+	if val == nil || *val != "foo" {
+		t.Errorf("val should be foo, got %v", val)
+	}
+
+	db.set("a", "bar")
+	n = db.count("bar")
+	if n != 1 {
+		t.Errorf("count should be 1, got %v", n)
+	}
+
+	db.begin()
+	n = db.count("bar")
+	if n != 1 {
+		t.Errorf("count should be 1, got %v", n)
+	}
+
+	db.delete("a")
+	val = db.get("a")
+	if val != nil {
+		t.Errorf("val should be nil, got %v", *val)
+	}
+
+	n = db.count("bar")
+	if n != 0 {
+		t.Errorf("count should be 0, got %v", n)
+	}
+
+	db.rollback()
+	val = db.get("a")
+	if val == nil || *val != "bar" {
+		t.Errorf("val should be bar, got %v", *val)
+	}
+
+	n = db.count("bar")
+	if n != 1 {
+		t.Errorf("count should be 1, got %v", n)
+	}
+
+	db.commit()
+
+	val = db.get("a")
+	if val == nil || *val != "bar" {
+		t.Errorf("val should be bar, got %v", *val)
+	}
+
+	val = db.get("b")
+	if val == nil || *val != "baz" {
+		t.Errorf("val should be baz, got %v", *val)
+	}
+}
