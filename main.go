f13a9a894ad6a98e7774bcb98e754fa95669b2a6
atec <root@atec.pub>
2019-03-05 11:20:28 -0500

    poc for get and set

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"os"
 	"strings"
 )
@@ -26,27 +27,45 @@ type database struct {
 	txs []tx
 }
 
+func (db database) get(name string) (*string, error) {
+	if val, ok := db.store[name]; ok {
+		return &val, nil
+	}
+	return nil, errors.New("not found")
+}
+
+func (db database) set(name, val string) error {
+	db.store[name] = val
+	return nil
+}
+
 type tx struct {
 }
 
+// TODO usage
 func err() {
 	println("invalid input")
 }
 
 func main() {
 
+	db := database{}
+	db.store = make(map[string]string)
+
 	for {
 		r := bufio.NewReader(os.Stdin)
 		in, _ := r.ReadString('\n')
 
-		args := strings.Split(in, " ")
+		stripped := strings.TrimSuffix(in, "\n")
+
+		args := strings.Split(stripped, " ")
 
 		switch len(args) {
 		case 1:
 			switch strings.ToLower(args[0]) {
-			case "begin\n":
-			case "rollback\n":
-			case "end\n": // TODO maybe strip trailing newline
+			case "begin":
+			case "rollback":
+			case "end": // TODO maybe strip trailing newline
 				os.Exit(0)
 			default:
 				err()
@@ -54,6 +73,13 @@ func main() {
 		case 2:
 			switch strings.ToLower(args[0]) {
 			case "get":
+				// pretty.Println(db.store)
+				val, err := db.get(args[1])
+				if err != nil {
+					println("<nil>") // TODO this could actually be a val
+				} else {
+					os.Stdout.Write([]byte(*val + "\n")) // TODO could be nil?
+				}
 			case "delete":
 			case "count":
 			default:
@@ -61,7 +87,10 @@ func main() {
 			}
 		case 3:
 			if strings.ToLower(args[0]) == "set" {
-
+				if err := db.set(args[1], args[2]); err != nil {
+					println("this shouldn't happen")
+				}
+				// pretty.Println(db.store)
 			} else {
 				err()
 			}
