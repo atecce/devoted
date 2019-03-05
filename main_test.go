b3ae874787b09c29960167a5ddcb4055e40ff031
atec <root@atec.pub>
2019-03-05 13:46:08 -0500

    test 2 passes

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func Test(t *testing.T) {
+func TestExample1(t *testing.T) {
 
 	db := database{}
 	db.store = make(map[string]string)
@@ -34,7 +34,7 @@ func Test(t *testing.T) {
 
 	n = db.count("foo")
 	if n != 1 {
-		t.Errorf("count should be 0, got %v", n)
+		t.Errorf("count should be 1, got %v", n)
 	}
 
 	db.set("b", "baz")
@@ -42,5 +42,38 @@ func Test(t *testing.T) {
 	val = db.get("B")
 	if val != nil {
 		t.Errorf("value should be nil, got %v", val)
+	}
+}
+
+func TestExample2(t *testing.T) {
+
+	db := database{}
+	db.store = make(map[string]string)
+	db.setBuf = make(map[string]string)
+
+	var val *string
+	var n uint
+
+	db.set("a", "foo")
+	db.set("a", "foo")
+	n = db.count("foo")
+	if n != 1 {
+		t.Errorf("count should be 1, got %v", n)
+	}
+
+	val = db.get("a")
+	if val == nil || *val != "foo" {
+		t.Errorf("val should be foo, got %v", val)
+	}
+
+	db.delete("a")
+	val = db.get("a")
+	if val != nil {
+		t.Errorf("val should be nil, got %v", val)
+	}
+
+	n = db.count("foo")
+	if n != 0 {
+		t.Errorf("count should be 0, got %v", n)
 	}
 }
